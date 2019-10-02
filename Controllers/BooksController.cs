df70a289cd007cf0cfef053d5509c5747f6407c8
Tomislav Bartolin <tomislav.bartolin@hotmail.com>
2019-10-02 15:12:09 +0100

    First commit

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,77 @@
+﻿using BooksApi.Models;
+using BooksApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BooksApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookService _bookService;
+
+        public BooksController(BookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        public ActionResult<Book> Get(string id)
+        {
+            var book = _bookService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+
+        [HttpGet("Search")]
+        public ActionResult<IEnumerable<Book>> Search(string term)
+        {
+           return _bookService.Search(term);
+                   
+        }
+
+        [HttpPost]
+        public ActionResult<Book> Create(Book book)
+        {
+            _bookService.Create(book);
+
+            return CreatedAtRoute("GetBook", new { id = book.Id.ToString() }, book);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Book bookIn)
+        {
+            var book = _bookService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _bookService.Update(id, bookIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var book = _bookService.Get(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _bookService.Remove(book.Id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ Services/BookService.cs
@@ -0,0 +1,95 @@
+﻿using BooksApi.Models;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+
+using MongoDB.Bson;
+
+namespace BooksApi.Services
+{
+    public class BookService
+    {
+        private readonly IMongoCollection<Book> _books;
+
+        public BookService(IBookstoreDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _books = database.GetCollection<Book>(settings.BooksCollectionName);
+        }
+
+        public List<Book> Get() =>
+            _books.Find(book => true).ToList();
+
+        public Book Get(string id) =>
+            _books.Find<Book>(book => book.Id == id).FirstOrDefault();
+
+        public Book Create(Book book)
+        {
+            _books.InsertOne(book);
+            return book;
+        }
+
+        public List<Book> Search(string term)
+        {
+
+            //_books.Aggregate()
+
+            //var aggregate = _books.Aggregate()
+            //    .se
+            //                          .Group(new BsonDocument { { "_id", "$token" }, { "count", new BsonDocument("$sum", 1) } })
+            //                          .Sort(new BsonDocument { { "count", -1 } })
+            //                          .Limit(10);
+
+
+            //var results = await aggregate.ToListAsync();
+
+
+            //{ "User", "Tom"},
+
+
+
+                var fuzzySearch = new BsonDocument
+                {
+                    {
+                        "$searchBeta", new BsonDocument
+                        {
+                            { 
+                                "term", new BsonDocument
+                                {
+                                    {"path", "Name"},
+                                    { "query", term },
+                                    { "fuzzy", new BsonDocument
+                                        {
+                                            {"maxEdits", 2 },
+                                            {"prefixLength", 0 }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                };
+
+            var pipeline = new BsonDocument[]
+            {
+                fuzzySearch
+            };
+
+            var result = _books.Aggregate<Book>(pipeline);
+
+            return result.ToList() ?? new List<Book>();
+
+        }
+
+        public void Update(string id, Book bookIn) =>
+            _books.ReplaceOne(book => book.Id == id, bookIn);
+
+        public void Remove(Book bookIn) =>
+            _books.DeleteOne(book => book.Id == bookIn.Id);
+
+        public void Remove(string id) =>
+            _books.DeleteOne(book => book.Id == id);
+    }
+}
